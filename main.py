e98ff5717a872af3e7ba91caa0c1fe689c795eb6
Marshall Lindsay <mblgh6@umsystem.edu>
2025-11-17 17:09:33 -0600

    Updates dockerfile to fully install SAM2. Adds working examples

--- main.py
+++ main.py
@@ -1,6 +1,88 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from PIL import Image
+from math import ceil
+import matplotlib.patches as patches
+from sam2.build_sam import build_sam2
+from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
+
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+
+
+def show_anns(anns):
+    if len(anns) == 0:
+        return
+    sorted_anns = sorted(anns, key=(lambda x: x['area']), reverse=True)
+    ax = plt.gca()
+    ax.set_autoscale_on(False)
+
+    img = np.ones((sorted_anns[0]['segmentation'].shape[0], sorted_anns[0]['segmentation'].shape[1], 4))
+    img[:,:,3] = 0
+    for ann in sorted_anns:
+        m = ann['segmentation']
+        color_mask = np.concatenate([np.random.random(3), [0.35]])
+        img[m] = color_mask
+    ax.imshow(img)
+
+
+def show_bboxes(image, bboxes):
+    fig, ax = plt.subplots()
+    ax.imshow(image)
+
+    for (x, y, w, h) in bboxes:
+        rect = patches.Rectangle(
+            (x, y), w, h,
+            linewidth=2,
+            edgecolor='red',
+            facecolor='none'
+        )
+        ax.add_patch(rect)
+
+    plt.axis('off')
+    plt.show()
+
+
 def main():
-    print("Hello from nautilus-sam2!")
+    image = Image.open('../data/USAF-1951.png')
+    image = np.array(image.convert("RGB"))
 
+    sam2_checkpoint = "/develop/code/sam2/checkpoints/sam2.1_hiera_large.pt"
+    model_cfg = "configs/sam2.1/sam2.1_hiera_l.yaml"
 
+    sam2 = build_sam2(model_cfg, sam2_checkpoint, device=device, apply_postprocessing=False)
+
+    mask_generator_2 = SAM2AutomaticMaskGenerator(
+        model=sam2,
+        points_per_side=64,
+        points_per_batch=128,
+        pred_iou_thresh=0.5,
+        stability_score_thresh=0.92,
+        stability_score_offset=0.7,
+        crop_n_layers=1,
+        box_nms_thresh=0.7,
+        crop_n_points_downscale_factor=2,
+        min_mask_region_area=100,
+        use_m2m=True,
+    )
+
+    masks2 = mask_generator_2.generate(image)
+    bboxes = [m['bbox'] for m in masks2]
+
+    show_bboxes(image, bboxes)
+    
 if __name__ == "__main__":
     main()
